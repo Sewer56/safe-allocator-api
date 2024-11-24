c0ebcefc55d08086fd414ca0825882bb811d400e
Sewer56 <sewer56lol@googlemail.com>
2024-11-24 13:37:32 +0000

    Added: 'nightly' feature for nightly allocator_api support

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,6 @@
 #![doc = include_str!("../README.MD")]
 #![cfg_attr(not(feature = "std"), no_std)]
+#![cfg_attr(feature = "nightly", feature(allocator_api))]
 extern crate alloc;
 
 pub mod raw_alloc;
